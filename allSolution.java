dc4434148cca51f716c408230099b9229d824e43
rajat-ds <rajatkumarbind@gmail.com>
2020-09-15 01:46:36 +0530

    merge two sorted arrays without using extra space

--- allSolution.java
+++ allSolution.java
@@ -42,3 +42,25 @@ public int findDuplicate(int[] nums) {
 		        index++;
 		    }
 		}
+		
+//333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333//////
+		
+//	Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+//Note:
+
+//The number of elements initialized in nums1 and nums2 are m and n respectively.
+//You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+          int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+    while (tail1 >= 0 && tail2 >= 0) {
+        nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? 
+                             nums1[tail1--] : nums2[tail2--];
+    }
+
+    while (tail2 >= 0) { //only need to combine with remaining nums2
+        nums1[finished--] = nums2[tail2--];
+    }
+    }
+		
