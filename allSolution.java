79eaa105782fb61fe5701062a8073b442ff208f7
rajat-ds <rajatkumarbind@gmail.com>
2020-09-17 01:00:26 +0530

    added merge interval in allSolution

--- allSolution.java
+++ allSolution.java
@@ -136,3 +136,43 @@ static void maxSubArraySum(int a[], int size)
         System.out.println("Starting index " + start); 
         System.out.println("Ending index " + end); 
     } 	
+	
+	////Merge Intervals 6 6666666666666666666666666666666666/////
+	  // The main function that takes a set of intervals, merges  
+    // overlapping intervals and prints the result  
+    public static void mergeIntervals(Interval arr[])  
+    {  
+        if (arr.length <= 0)  
+            return;  
+    
+        Stack<Interval> stack=new Stack<>(); 
+         Arrays.sort(arr,new Comparator<Interval>(){ 
+            public int compare(Interval i1,Interval i2) 
+            { 
+                return i1.start-i2.start; 
+            } 
+        }); 
+          stack.push(arr[0]);  
+           for (int i = 1 ; i < arr.length; i++)  
+        {  
+              Interval top = stack.peek();  
+    
+            if (top.end < arr[i].start)  
+                stack.push(arr[i]);  
+      
+            else if (top.end < arr[i].end)  
+            {  
+                top.end = arr[i].end;  
+                stack.pop();  
+                stack.push(top);  
+            }  
+        }  
+    
+        // Print contents of stack  
+        System.out.print("The Merged Intervals are: "); 
+        while (!stack.isEmpty())  
+        {  
+            Interval t = stack.pop();  
+            System.out.print("["+t.start+","+t.end+"] "); 
+        }   
+    }
