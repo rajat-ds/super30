a1aff733fca281685ba7176a83c65773c0497773
rajat-ds <rajatkumarbind@gmail.com>
2020-09-25 21:53:55 +0530

    added in all solution rotate matrix

--- allSolution.java
+++ allSolution.java
@@ -365,6 +365,36 @@ class StockBuySell {
         return; 
     }
 	
+	/// 12 rotate Matrix 
+	
+	   static void rotateMatrix( 
+        int N, int mat[][]) 
+    { 
+        // Consider all squares one by one 
+        for (int x = 0; x < N / 2; x++) { 
+            // Consider elements in group 
+            // of 4 in current square 
+            for (int y = x; y < N - x - 1; y++) { 
+                // Store current cell in 
+                // temp variable 
+                int temp = mat[x][y]; 
+  
+                // Move values from right to top 
+                mat[x][y] = mat[y][N - 1 - x]; 
+  
+                // Move values from bottom to right 
+                mat[y][N - 1 - x] 
+                    = mat[N - 1 - x][N - 1 - y]; 
+  
+                // Move values from left to bottom 
+                mat[N - 1 - x][N - 1 - y] = mat[N - 1 - y][x]; 
+  
+                // Assign temp to left 
+                mat[N - 1 - y][x] = temp; 
+            } 
+        } 
+    }
+	
 	
 	
 	/////13 seacrh 2 D matrxi 
