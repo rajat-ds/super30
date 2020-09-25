fe015477a5b5b9f308104e57322a91d55af18fe6
rajat-ds <rajatkumarbind@gmail.com>
2020-09-25 21:57:15 +0530

    added poer of Xn in all solution

--- allSolution.java
+++ allSolution.java
@@ -464,3 +464,23 @@ class StockBuySell {
 	        // if not found 
 	        return false; 
 	    } 
+		
+		
+		////// 14 power of xn 
+		  static float power(float x, int y) 
+	    { 
+	        float temp; 
+	        if( y == 0) 
+	            return 1; 
+	        temp = power(x, y/2);  
+	          
+	        if (y%2 == 0) 
+	            return temp*temp; 
+	        else
+	        { 
+	            if(y > 0) 
+	                return x * temp * temp; 
+	            else
+	                return (temp * temp) / x; 
+	        } 
+	    }  
